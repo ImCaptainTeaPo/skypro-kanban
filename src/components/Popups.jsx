c5a34a1d6b7ab68386703473312ba9cb623ea20f
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-05-28 18:21:05 +0300

    PopUser

--- src/components/Popups.jsx
+++ src/components/Popups.jsx
@@ -1,27 +1,10 @@
 import Calendar from "./Calendar/Calendar";
+import PopUser from "./PopUser/PopUser";
 
 function Popups() {
   return (
     <>
-      <div className="pop-exit" id="popExit">
-        <div className="pop-exit__container">
-          <div className="pop-exit__block">
-            <div className="pop-exit__ttl">
-              <h2>Выйти из аккаунта?</h2>
-            </div>
-            <form className="pop-exit__form" id="formExit" action="#">
-              <div className="pop-exit__form-group">
-                <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                  <a href="modal/signin.html">Да, выйти</a>{" "}
-                </button>
-                <button className="pop-exit__exit-no _hover03" id="exitNo">
-                  <a href="main.html">Нет, остаться</a>{" "}
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <PopUser />
 
       <div className="pop-new-card" id="popNewCard">
         <div className="pop-new-card__container">
