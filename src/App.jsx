3afde26120fc708bd748fb393beb06367cdb2801
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-05-28 19:21:40 +0300

    structure update

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Popups from "./components/Popups";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Popups from "./components/Popups/Popups";
 
 function App() {
   const [count, setCount] = useState(0);
