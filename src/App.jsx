94005e9906686e92cc9a7f763bb57095b1f27471
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-06-21 12:36:21 +0300

    shared.styled+global.styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Popups from "./components/Popups/Popups";
+import GlobalStyles from "./styles/GlobalStyles";
+import { Wrapper } from "./components/shared.styled";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="wrapper">
-      {/* pop-up start */}
-
-      <Popups />
-
-      {/* pop-up end */}
-
-      <Header />
-
-      <Main />
-    </div>
+    <>
+      <GlobalStyles />
+      <Wrapper>
+        <Popups />
+        <Header />
+        <Main />
+      </Wrapper>
+    </>
   );
 }
 
---
+++ src/components/shared.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1220px;
+  margin: 0 auto;
+  padding: 0 15px;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: #ffffff;
+`;
