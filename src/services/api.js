7b3d8047eb705acf7086c5f49695d9d48b08d76b
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-07-28 18:48:16 +0300

    fix error "Content-Type"

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,3 @@
-// src/services/api.js
 import axios from "axios";
 
 const API_URL = "https://wedev-api.sky.pro/api/kanban";
@@ -23,10 +22,10 @@ export async function addTask(token, taskData) {
     const response = await axios.post(API_URL, taskData, {
       headers: {
         Authorization: `Bearer ${token}`,
-        // "Content-Type": "application/json",
+        "Content-Type": " ",
       },
     });
-    return response.data.task;
+    return response.data.tasks;
   } catch (error) {
     throw new Error(
       error.response?.data?.error || "Ошибка при добавлении задачи"
@@ -36,13 +35,13 @@ export async function addTask(token, taskData) {
 
 export async function updateTask(token, id, taskData) {
   try {
-    const response = await axios.patch(`${API_URL}/${id}`, taskData, {
+    const response = await axios.put(`${API_URL}/${id}`, taskData, {
       headers: {
         Authorization: `Bearer ${token}`,
-        // "Content-Type": "application/json",
+        "Content-Type": " ",
       },
     });
-    return response.data.task;
+    return response.data.tasks;
   } catch (error) {
     throw new Error(
       error.response?.data?.error || "Ошибка при обновлении задачи"
@@ -52,11 +51,12 @@ export async function updateTask(token, id, taskData) {
 
 export async function deleteTask(token, id) {
   try {
-    await axios.delete(`${API_URL}/${id}`, {
+    const response = await axios.delete(`${API_URL}/${id}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
+    return response.data.tasks;
   } catch (error) {
     throw new Error(
       error.response?.data?.error || "Ошибка при удалении задачи"
--- src/services/auth.js
+++ src/services/auth.js
@@ -4,7 +4,15 @@ const AUTH_URL = "https://wedev-api.sky.pro/api/user";
 
 export async function signUp({ name, login, password }) {
   try {
-    const response = await axios.post(AUTH_URL, { name, login, password });
+    const response = await axios.post(
+      AUTH_URL,
+      { name, login, password },
+      {
+        headers: {
+          "Content-Type": " ",
+        },
+      }
+    );
     return response.data.user;
   } catch (error) {
     throw new Error(error.response?.data?.error || "Ошибка регистрации");
@@ -13,7 +21,15 @@ export async function signUp({ name, login, password }) {
 
 export async function signIn({ login, password }) {
   try {
-    const response = await axios.post(`${AUTH_URL}/login`, { login, password });
+    const response = await axios.post(
+      `${AUTH_URL}/login`,
+      { login, password },
+      {
+        headers: {
+          "Content-Type": " ",
+        },
+      }
+    );
     return response.data.user;
   } catch (error) {
     throw new Error(error.response?.data?.error || "Ошибка авторизации");
