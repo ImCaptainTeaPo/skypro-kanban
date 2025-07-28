dd10cd515031ce1adbd4efd549d7bc9330abf855
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-07-28 18:03:03 +0300

    Add api.js auth.js

---
+++ src/services/auth.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const AUTH_URL = "https://wedev-api.sky.pro/api/user";
+
+export async function signUp({ name, login, password }) {
+  try {
+    const response = await axios.post(AUTH_URL, { name, login, password });
+    return response.data.user;
+  } catch (error) {
+    throw new Error(error.response?.data?.error || "Ошибка регистрации");
+  }
+}
+
+export async function signIn({ login, password }) {
+  try {
+    const response = await axios.post(`${AUTH_URL}/login`, { login, password });
+    return response.data.user;
+  } catch (error) {
+    throw new Error(error.response?.data?.error || "Ошибка авторизации");
+  }
+}
