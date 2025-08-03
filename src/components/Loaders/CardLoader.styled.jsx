defb45a81bce5ffbc275d932f23965373d2e1029
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-08-03 13:02:36 +0300

    Update Loaders

--- src/components/Loaders/CardLoader.styled.jsx
+++ src/components/Loaders/CardLoader.styled.jsx
@@ -1,27 +1,26 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const ScardLoader = styled.div`
-  background: #e6e6e6;
-  border-radius: 10px;
-  padding: 15px 17px 19px 13px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  width: 220px;
-`;
+	background: ${({ theme }) => theme.colors.cardBg};
+	border-radius: 10px;
+	padding: 15px 17px 19px 13px;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	width: 220px;
+`
 
 export const ScardLoaderContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-`;
-
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+`
 export const ScardLoaderPretitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`
 
 export const ScardLoaderTitle = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
+	display: flex;
+	flex-direction: column;
+	gap: 32px;
+`
--- src/components/Loaders/Loader.jsx
+++ src/components/Loaders/Loader.jsx
@@ -1,18 +1,20 @@
-import { Cloader } from "./Loader.styled";
+import { useTheme } from 'styled-components'
+import { Cloader } from './Loader.styled'
 
 const Loader = ({ width = 220, height = 130 }) => {
-  const colorStart = "#c1cddc";
-  const colorEnd = "#e9eef7";
+	const theme = useTheme()
+	const colorStart = theme.loaderStart || '#c1cddc'
+	const colorEnd = theme.loaderEnd || '#e9eef7'
 
-  return (
-    <Cloader
-      style={{
-        width: `${width}px`,
-        height: `${height}px`,
-        background: `linear-gradient(90deg, ${colorStart}, ${colorEnd})`,
-      }}
-    ></Cloader>
-  );
-};
+	return (
+		<Cloader
+			style={{
+				width: `${width}px`,
+				height: `${height}px`,
+				background: `linear-gradient(90deg, ${colorStart}, ${colorEnd})`,
+			}}
+		></Cloader>
+	)
+}
 
-export default Loader;
+export default Loader
