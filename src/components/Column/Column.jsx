c09e5f8c8b1855d71e6f538be0e98ba871d02664
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-08-03 13:00:42 +0300

    Update Colomn

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,59 +1,32 @@
-import Card from "../Card/Card";
-import Loader from "../Loaders/Loader";
-import {
-  ColumnWrapper,
-  ColumnTitle,
-  ColumnTitleText,
-  CardsContainer,
-} from "./Column.styled";
+import { useContext } from 'react'
+import { useDrop } from 'react-dnd'
+import { CardContext } from '../../context/CardContext'
+import Cards from '../Card/Cards'
+import { Scolumn, ScolumnTitle } from './Column.styled'
 
-function Column({ title, cards, loading }) {
-  return (
-    <ColumnWrapper>
-      <ColumnTitle>
-        <ColumnTitleText>{title}</ColumnTitleText>
-      </ColumnTitle>
-      <CardsContainer>
-        {cards && cards.length > 0 ? (
-          cards.map((card) => {
-            if (loading) {
-              return <Loader key={card._id} />;
-            } else {
-              return (
-                <Card
-                  key={card._id}
-                  _id={card._id}
-                  topic={card.topic}
-                  title={card.title}
-                  date={card.date}
-                />
-              );
-            }
-          })
-        ) : (
-          <p style={{ padding: "10px", fontStyle: "italic" }}>Нет задач</p>
-        )}
-      </CardsContainer>
-    </ColumnWrapper>
-  );
+export default function Column({ title, cards }) {
+	const { editTask } = useContext(CardContext)
+	const [{ isOver }, dropRef] = useDrop({
+		accept: 'CARD',
+		drop: item => {
+			editTask(item.id, { status: title })
+		},
+		collect: monitor => ({
+			isOver: monitor.isOver(),
+		}),
+	})
+
+	return (
+		<Scolumn
+			ref={dropRef}
+			style={{
+				border: isOver ? 'none' : 'none',
+			}}
+		>
+			<ScolumnTitle>
+				<p>{title}</p>
+			</ScolumnTitle>
+			<Cards cards={cards} />
+		</Scolumn>
+	)
 }
-
-export default Column;
-
-// {cards && cards.length > 0 ? (
-//           cards.map((card) => {
-//             loading ? (
-//               <Loader />
-//             ) : (
-//               <Card
-//                 _id={card._id}
-//                 key={card._id}
-//                 topic={card.topic}
-//                 title={card.title}
-//                 date={card.date}
-//               />
-//             );
-//           })
-//         ) : (
-//           <p style={{ padding: "10px", fontStyle: "italic" }}>Нет задач</p>
-//         )}
