11e303656af53b8689cfe4ba4e27d511dee9e9f3
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-08-03 12:54:42 +0300

    Create topicMapping update app.jsx main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
-import { GlobalWrapper } from "./components/shared.styled";
-import AppRoutes from "./AppRoutes";
-import AuthProvider from "./context/AuthProvider";
-import TaskProvider from "./context/TaskProvider";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { ToastContainer } from 'react-toastify'
+import AppRoutes from './components/AppRoutes/AppRoutes'
+import AuthProvider from './context/AuthProvider'
+import { CardProvider } from './context/CardProvider'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <TaskProvider>
-          <GlobalStyles />
-          <GlobalWrapper>
-            <AppRoutes />
-          </GlobalWrapper>
-        </TaskProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  );
+	return (
+		<AuthProvider>
+			<CardProvider>
+				<DndProvider backend={HTML5Backend}>
+					<AppRoutes />
+					<ToastContainer
+						position='bottom-right'
+						hideProgressBar
+						closeOnClick
+						draggable
+						pauseOnHover
+						toastClassName={() => 'custom-toast'}
+					/>
+				</DndProvider>
+			</CardProvider>
+		</AuthProvider>
+	)
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./app.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { ThemeProviderCustom } from './context/ThemeProvider.jsx'
+import { GlobalStyles } from './styles/GlobalStyles.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<BrowserRouter>
+			<ThemeProviderCustom>
+				<GlobalStyles />
+				<App />
+			</ThemeProviderCustom>
+		</BrowserRouter>
+	</StrictMode>
+)
