ca4df102e8fed27a03ccfd023577f03e2f2bbd66
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-08-03 13:11:40 +0300

    Update services

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,37 +1,41 @@
-import axios from "axios";
+import axios from 'axios'
+import { USER_API_URL } from './apiConfig'
 
-const AUTH_URL = "https://wedev-api.sky.pro/api/user";
+export async function signIn(userData) {
+	try {
+		const response = await axios.post(`${USER_API_URL}/login`, userData, {
+			headers: {
+				'Content-Type': '',
+			},
+		})
+		const user = response.data.user
+		const token = user.token
 
-export async function signUp({ name, login, password }) {
-  try {
-    const response = await axios.post(
-      AUTH_URL,
-      { name, login, password },
-      {
-        headers: {
-          "Content-Type": " ",
-        },
-      }
-    );
-    return response.data.user;
-  } catch (error) {
-    throw new Error(error.response?.data?.error || "Ошибка регистрации");
-  }
+		localStorage.setItem('token', token)
+		return { user, token }
+	} catch (error) {
+		throw new Error(error.response?.data?.error || 'Ошибка входа')
+	}
 }
 
-export async function signIn({ login, password }) {
-  try {
-    const response = await axios.post(
-      `${AUTH_URL}/login`,
-      { login, password },
-      {
-        headers: {
-          "Content-Type": " ",
-        },
-      }
-    );
-    return response.data.user;
-  } catch (error) {
-    throw new Error(error.response?.data?.error || "Ошибка авторизации");
-  }
+export async function signUp(userData) {
+	try {
+		const response = await axios.post(USER_API_URL, userData, {
+			headers: {
+				'Content-Type': '',
+			},
+		})
+		const user = response.data.user
+		const token = user.token
+
+		localStorage.setItem('token', token)
+		return { user, token }
+	} catch (error) {
+		console.log(error)
+		throw new Error(error.response?.data?.error || 'Ошибка регистрации')
+	}
+}
+
+export function getToken() {
+	return localStorage.getItem('token')
 }
